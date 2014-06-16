89fe6a15220145cb8abe641161f5be56fb1c103c
Thordur Bjornsson <thorduri@secnorth.net>
2014-06-16 11:03:52 +0200

    Fix copy pasto in getchef.sh

--- packer/getchef.sh
+++ packer/getchef.sh
@@ -4,5 +4,5 @@ cd httpdir
 test -f chef-11.12.8-2.el5.x86_64.rpm || \
 wget -q --no-check-certificate https://opscode-omnibus-packages.s3.amazonaws.com/el/5/x86_64/chef-11.12.8-2.el5.x86_64.rpm
 
-test -f --no-check-certificate chef-server-11.1.1-1.el5.x86_64.rpm || \
-wget https://opscode-omnibus-packages.s3.amazonaws.com/el/5/x86_64/chef-server-11.1.1-1.el5.x86_64.rpm
+test -f chef-server-11.1.1-1.el5.x86_64.rpm || \
+wget --no-check-certificate https://opscode-omnibus-packages.s3.amazonaws.com/el/5/x86_64/chef-server-11.1.1-1.el5.x86_64.rpm
